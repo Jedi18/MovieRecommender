f854fddac9968606bc6c5e3a5409eb2ef1b40ee5
Jedi18 <targetakhil@gmail.com>
2020-11-15 20:05:38 +0530

    added search and bootstrap

--- recommender/views.py
+++ recommender/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Movie Recommender")
+    return render(request, "recommender/index.html")
+
+def search(request):
+    if request.GET.get('title'):
+        movietitle = request.GET.get('title')
+        movieslist = Movie.objects.filter(title__contains=movietitle).all()[:10]
+        return render(request, "recommender/search.html", {'movieslist' : movieslist})
+    else:
+        return render(request, "recommender/index.html")
